64ccc517f990101561fc466215aaa1bd77f824e2
Think-plus Outlook <146562490+Jose-ctr@users.noreply.github.com>
2026-08-04 01:09:41 +0300

    Add Student class with initialization parameters

---
+++ models/student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, id, name, admission_no, parent_phone, total_fee, balance, admission_date):
+        self.id = id
+        self.name = name
+        self.admission_no = admission_no
+        self.parent_phone = parent_phone
+        self.total_fee = total_fee
+        self.balance = balance
+        self.admission_date = admission_date
