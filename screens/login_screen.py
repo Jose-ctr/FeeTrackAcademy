b7c342baf8ffeb58c6067b3f2a58fe5a76aa60bb
Think-plus Outlook <146562490+Jose-ctr@users.noreply.github.com>
2026-08-04 00:01:39 +0300

    Add initial LoginScreen implementation

---
+++ screens/login_screen.py
@@ -0,0 +1,12 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+from utils.popup import show_popup
+
+
+class LoginScreen(Screen):
+
+    def
