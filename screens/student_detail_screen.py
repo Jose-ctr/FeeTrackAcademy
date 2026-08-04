b9a5d151c30d8c278dd898d33c17baadbc26b39f
Think-plus Outlook <146562490+Jose-ctr@users.noreply.github.com>
2026-08-04 17:44:49 +0300

    Implement StudentDetailScreen with layout and back button

--- screens/student_detail_screen.py
+++ screens/student_detail_screen.py
@@ -1,1 +1,23 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 
+
+class StudentDetailScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
+
+        layout.add_widget(Label(text='Student Details', font_size=24))
+        layout.add_widget(Label(text='Name: -'))
+        layout.add_widget(Label(text='Class: -'))
+        layout.add_widget(Label(text='Balance: -'))
+
+        back_btn = Button(text='Back')
+        back_btn.bind(on_press=lambda x: setattr(self.manager, 'current', 'student_list'))
+
+        layout.add_widget(back_btn)
+
+        self.add_widget(layout)
