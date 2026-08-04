ffa74de24cb1d2b1aa351679edbcaab7e16cee85
Think-plus Outlook <146562490+Jose-ctr@users.noreply.github.com>
2026-08-04 17:37:52 +0300

    Import multiple screen modules in init.py

--- screens/*init*.py
+++ screens/*init*.py
@@ -1,1 +1,6 @@
-
+from .login_screen import LoginScreen
+from .dashboard_screen import DashboardScreen
+from .student_list_screen import StudentListScreen
+from .add_student_screen import AddStudentScreen
+from .student_detail_screen import StudentDetailScreen
+from .payment_screen import PaymentScreen
