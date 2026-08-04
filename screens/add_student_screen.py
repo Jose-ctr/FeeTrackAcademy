a9f87b18093cb3fa7832e3a1307f2d27a6401053
Think-plus Outlook <146562490+Jose-ctr@users.noreply.github.com>
2026-08-04 17:40:04 +0300

    Implement Add Student Screen with inputs and buttons

--- screens/add_student_screen.py
+++ screens/add_student_screen.py
@@ -1,1 +1,30 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
+
+class AddStudentScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
+
+        layout.add_widget(Label(text='Add Student', font_size=24))
+
+        self.name_input = TextInput(hint_text='Student Name')
+        self.class_input = TextInput(hint_text='Class')
+
+        layout.add_widget(self.name_input)
+        layout.add_widget(self.class_input)
+
+        save_btn = Button(text='Save Student')
+        back_btn = Button(text='Back')
+
+        back_btn.bind(on_press=lambda x: setattr(self.manager, 'current', 'dashboard'))
+
+        layout.add_widget(save_btn)
+        layout.add_widget(back_btn)
+
+        self.add_widget(layout)
